323cc5a78f50f8693ad86f94b2271a0d7b3c2f6c
Cathay <dodeux81@gmail.com>
2026-02-01 14:45:10 +0900

    Implement upload widget

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,12 @@ import {useNotificationProvider} from "./components/refine-ui/notification/use-n
 import {ThemeProvider} from "./components/refine-ui/theme/theme-provider";
 import {dataProvider} from "./providers/data";
 import Dashboard from "@/pages/dashboard.tsx";
-import {BookOpen, HomeIcon,} from "lucide-react";
+import {BookOpen, GraduationCap, HomeIcon,} from "lucide-react";
 import {Layout} from "@/components/refine-ui/layout/layout.tsx";
 import SubjectsList from "@/pages/subjects/list.tsx";
 import SubjectCreate from "@/pages/subjects/create.tsx";
+import ClassesList from "@/pages/classes/list.tsx";
+import ClassesCreate from "@/pages/classes/create.tsx";
 
 function App() {
     return (
@@ -42,6 +44,12 @@ function App() {
                                     list: '/subjects',
                                     create: '/subjects/create',
                                     meta: {label: 'Subjects', icon: <BookOpen/>}
+                                },
+                                {
+                                    name: 'classes',
+                                    list: '/classes',
+                                    create: '/classes/create',
+                                    meta: {label: 'Classes', icon: <GraduationCap/>}
                                 }
                             ]}
                         >
@@ -55,6 +63,11 @@ function App() {
                                     <Route path="/subjects">
                                         <Route index element={<SubjectsList/>}></Route>
                                         <Route path="create" element={<SubjectCreate/>}/>
+                                    </Route>
+
+                                    <Route path="/classes">
+                                        <Route index element={<ClassesList/>}></Route>
+                                        <Route path="create" element={<ClassesCreate/>}/>
                                     </Route>
                                 </Route>
 
